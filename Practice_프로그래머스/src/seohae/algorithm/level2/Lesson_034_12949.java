0bf0254f369bf6ce3872757150ae4d8e2816d79f
seohae <seohae0406@gmail.com>
2021-09-03 20:57:48 +0900

    [09/03]

--- Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_034_12949.java
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_034_12949.java
@@ -1,5 +1,7 @@
 package seohae.algorithm.level2;
 
+import java.util.Arrays;
+
 /**
  * 행렬의 곱셈
  * https://programmers.co.kr/learn/courses/30/lessons/12949
@@ -8,13 +10,26 @@ package seohae.algorithm.level2;
 public class Lesson_034_12949 {
     public static void main(String[] args) {
         Lesson_034_12949 lesson = new Lesson_034_12949();
-        //System.out.println(lesson.solution("3people unFollowed  me"));
+
+        int[][] a = new int[][] { {1,4}, {3,2}, {4,1} };
+        int[][] b = new int[][] { {3,3}, {3,3}};
+        System.out.println(Arrays.deepToString(lesson.solution(a, b)));
     }
 
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = {};
+        int[][] answer = new int[arr1.length][arr2[0].length];
 
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                int sum = 0;
 
+                for(int k = 0; k < arr2.length; k++) {
+                    sum += arr1[i][k] * arr2[k][j];
+                }
+
+                answer[i][j] = sum;
+            }
+        }
 
         return answer;
     }
