14ef78baaa4642978f89041d458752a0129502c4
seohae <seohae0406@gmail.com>
2021-09-04 10:13:45 +0900

    [09/04]

---
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_035_12945.java
@@ -0,0 +1,41 @@
+package seohae.algorithm.level2;
+
+import java.util.Arrays;
+
+/**
+ * 피보나치 수
+ * https://programmers.co.kr/learn/courses/30/lessons/12945
+ */
+
+public class Lesson_035_12945 {
+    public static void main(String[] args) {
+        Lesson_035_12945 lesson = new Lesson_035_12945();
+
+        System.out.println(lesson.solution(5));
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    /**
+     * 재귀 - 시간초과
+     */
+//    public int solution(int n) {
+//        int answer = fibo(n);
+//
+//        return answer;
+//    }
+//
+//    public static int fibo(int n) {
+//        if (n > 1) {
+//            return fibo(n - 2) + fibo(n - 1);
+//        } else if (n == 1){
+//            return 1;
+//        } else {
+//            return 0;
+//        }
+//    }
+}
