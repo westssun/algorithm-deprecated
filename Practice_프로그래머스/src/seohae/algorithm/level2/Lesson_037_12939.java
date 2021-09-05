4b20eaf06aa36aea627bd85007be1151e08deed4
seohae <seohae0406@gmail.com>
2021-09-05 16:18:46 +0900

    [09/05]

---
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_037_12939.java
@@ -0,0 +1,32 @@
+package seohae.algorithm.level2;
+
+/**
+ * 최댓값과 최솟값
+ * https://programmers.co.kr/learn/courses/30/lessons/12939
+ */
+
+public class Lesson_037_12939 {
+    public static void main(String[] args) {
+        Lesson_037_12939 lesson = new Lesson_037_12939();
+
+        System.out.println(lesson.solution("1 2 3 4"));
+    }
+
+    public String solution(String s) {
+        String answer = "";
+
+        String[] arr = s.split(" ");
+
+        int min_value = Integer.MAX_VALUE;
+        int max_value = Integer.MIN_VALUE;
+
+        for (int j = 0; j < arr.length; j++) {
+            min_value = Math.min(min_value, Integer.parseInt(arr[j]));
+            max_value = Math.max(max_value, Integer.parseInt(arr[j]));
+        }
+
+        answer = min_value + " " + max_value;
+
+        return answer;
+    }
+}
---
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_038_12924.java
@@ -0,0 +1,37 @@
+package seohae.algorithm.level2;
+
+/**
+ * 최댓값과 최솟값
+ * https://programmers.co.kr/learn/courses/30/lessons/12939
+ */
+
+public class Lesson_038_12924 {
+    public static void main(String[] args) {
+        Lesson_038_12924 lesson = new Lesson_038_12924();
+
+        System.out.println(lesson.solution(15));
+    }
+
+    public int solution(int n) {
+        int answer = 1; /* 자기 자신은 무조건 포함이므로 1부터 시작 (n 일때 n 도 포함된다) */
+
+        /* 1부터 시작 */
+        for (int i = 1; i < n; i++) {
+            int sum = i;
+
+            /* i + 1 부터 n 포함 까지 */
+            for (int j = i + 1; j <= n; j++) {
+                sum += j;
+
+                if (sum == n) { /* n 일 경우 */
+                    answer += 1;
+                    break;
+                } else if (sum > n) { /* sum 이 n 보다 커진 이후로는 해당 i의 경우가 없다고 판단 */
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
