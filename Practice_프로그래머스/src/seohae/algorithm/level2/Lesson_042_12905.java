072213245f01ddc068b44333fd0b8fbd1be71d3e
seohae <seohae0406@gmail.com>
2021-09-08 13:48:15 +0900

    [09/08]

---
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_042_12905.java
@@ -0,0 +1,26 @@
+package seohae.algorithm.level2;
+
+import java.util.Stack;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12905
+ */
+
+public class Lesson_042_12905 {
+    public static void main(String[] args) {
+        Lesson_042_12905 lesson = new Lesson_042_12905();
+
+        int[][] a = new int[][] { {0,1,1,1}, {1,1,1,1}, {1,1,1,1}, {0,0,1,0}};
+        System.out.println(lesson.solution(a));
+    }
+
+    public int solution(int [][]board)
+    {
+        int answer = 1234;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
---
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_043_42584.java
@@ -0,0 +1,24 @@
+package seohae.algorithm.level2;
+
+import java.util.Arrays;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/42584
+ */
+
+public class Lesson_043_42584 {
+    public static void main(String[] args) {
+        Lesson_043_42584 lesson = new Lesson_043_42584();
+        int[] a = new int[]{1,2};
+
+        System.out.println(Arrays.toString(lesson.solution(a)));
+    }
+
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+}
