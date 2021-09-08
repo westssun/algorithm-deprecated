0d69a4debd0dbca444a31c878d19020bd9fbdf34
seohae <seohae0406@gmail.com>
2021-09-08 13:52:40 +0900

    [09/08]

--- Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_043_42584.java
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_043_42584.java
@@ -9,15 +9,30 @@ import java.util.Arrays;
 public class Lesson_043_42584 {
     public static void main(String[] args) {
         Lesson_043_42584 lesson = new Lesson_043_42584();
-        int[] a = new int[]{1,2};
+        int[] a = new int[]{1,2,3,2,3};
 
         System.out.println(Arrays.toString(lesson.solution(a)));
     }
 
     public int[] solution(int[] prices) {
-        int[] answer = {};
+        int[] answer = new int[prices.length];
 
+        for (int i = 0; i < prices.length; i++) {
+            int current = prices[i];
 
+            int cnt = 0;
+            for (int j = i + 1; j < prices.length; j++) {
+                if (current > prices[j]) {
+                    cnt += 1;
+                    answer[i] = cnt;
+                    break;
+                } else {
+                    cnt += 1;
+                }
+
+                answer[i] = cnt;
+            }
+        }
 
         return answer;
     }
