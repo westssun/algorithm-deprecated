0f374c889558e45d0afcaaab4063784fc4152f3a
seohae <seohae0406@gmail.com>
2021-09-18 15:53:11 +0900

    09/18

---
+++ Practice_백준_문제풀이/src/seohae/algorithm/level1/Problem_007_1260.java
@@ -0,0 +1,93 @@
+package seohae.algorithm.level1;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * https://www.acmicpc.net/problem/1260
+ */
+public class Problem_007_1260 {
+    static int graph[][];
+    static boolean[] visited;
+    static int n; /* 첫째 줄의 정점 개수 */
+    static int m; /* 간선 개수 */
+    static int v; /* 탐색을 시작할 정점의 번호 */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s," ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+
+        graph = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
+
+        for (int j = 0; j < n + 1; j++) {
+            Arrays.fill(graph[j], 0);
+        }
+
+        Arrays.fill(visited, false);
+
+        for (int i = 0; i < m; i++) {
+            String edge = br.readLine();
+            StringTokenizer st1 = new StringTokenizer(edge," ");
+
+            int a = Integer.parseInt(st1.nextToken());
+            int b = Integer.parseInt(st1.nextToken());
+
+            graph[a][b] = 1;
+            graph[b][a] = 1;
+        }
+
+        /* dfs */
+        dfs(v);
+
+        System.out.println();
+
+        /* bfs */
+        Arrays.fill(visited, false);
+        bfs(v);
+    }
+
+    static void dfs(int start) {
+        /* 방문 */
+        visited[start] = true;
+        System.out.print(start + " ");
+
+        for (int i = 1; i < graph[start].length; i++) {
+            /* 인정행렬일 경우 */
+            if (graph[start][i] == 1) {
+                if (!visited[i]) {
+                    dfs(i);
+                }
+            }
+        }
+    }
+
+    static void bfs(int start) {
+        /* 방문 */
+        visited[start] = true;
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            int target = queue.poll();
+            System.out.print(target + " ");
+
+            for (int i = 1; i < graph[target].length; i++) {
+                if (graph[target][i] == 1) {
+                    if (!visited[i]) {
+                        queue.offer(i);
+                        visited[i] = true;
+                    }
+                }
+            }
+        }
+
+    }
+}
