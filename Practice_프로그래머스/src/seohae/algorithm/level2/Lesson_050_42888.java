6bd7a5fe4bf0456c2109bad20657d6bfe9283bbd
seohae <seohae0406@gmail.com>
2021-09-24 21:36:35 +0900

    09/24 오픈채팅방

--- Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_050_42888.java
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_050_42888.java
@@ -1,7 +1,6 @@
 package seohae.algorithm.level2;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/42888
@@ -16,8 +15,46 @@ public class Lesson_050_42888 {
     }
 
     public String[] solution(String[] record) {
-        String[] answer = {};
+        /* key: userId, value: 닉네임을 셋팅할 Map 선언 */
+        Map<String, String> member = new HashMap<>();
+        /* 최종적으로 출력을 위해 담을 queue */
+        Queue<String> queue = new LinkedList<>();
 
+        /* 파라미터 record 반복문 실행 */
+        for (String target : record) {
+            String[] targetArr = target.split(" ");
+            String startWord = targetArr[0];
+
+            /* 키워드에 따른 분기 처리 */
+            if ("Enter".equals(startWord)) {
+                member.put(targetArr[1], targetArr[2]);
+                queue.offer(target);
+            } else if ("Leave".equals(startWord)) {
+                queue.offer(target);
+            } else if ("Change".equals(startWord)) {
+                /* 같은 key 일 경우 value change */
+                member.put(targetArr[1], targetArr[2]);
+            }
+        }
+
+        String[] answer = new String[queue.size()];
+        int index = 0;
+
+        /* queue 만큼 반복문 */
+        while (!queue.isEmpty()) {
+            String target = queue.poll();
+            String[] targetArr = target.split(" ");
+            String startWord = targetArr[0];
+
+            /* 출력 */
+            if ("Enter".equals(startWord)) {
+                answer[index] = member.get(targetArr[1]) + "님이 들어왔습니다.";
+            } else if ("Leave".equals(startWord)) {
+                answer[index] = member.get(targetArr[1]) + "님이 나갔습니다.";
+            }
+
+            index++;
+        }
 
         return answer;
     }
