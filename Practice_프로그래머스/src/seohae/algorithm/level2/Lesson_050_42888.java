8539323b15042924f99bbc4ef63e7ad4d71c68da
seohae <seohae0406@gmail.com>
2021-09-24 12:29:29 +0900

    09/24 프로그래머스 50번째 문제 선택

---
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_050_42888.java
@@ -0,0 +1,24 @@
+package seohae.algorithm.level2;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/42888
+ */
+
+public class Lesson_050_42888 {
+    public static void main(String[] args) {
+        Lesson_050_42888 lesson = new Lesson_050_42888();
+
+        String[] a = new String[]{"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+        System.out.println(Arrays.toString(lesson.solution(a)));
+    }
+
+    public String[] solution(String[] record) {
+        String[] answer = {};
+
+
+        return answer;
+    }
+}
