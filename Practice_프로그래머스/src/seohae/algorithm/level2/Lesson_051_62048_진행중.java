cadf7fdf023d41a63eeef6e4ae7d6751d689b59e
seohae <seohae0406@gmail.com>
2021-09-29 18:54:10 +0900

    0929

--- Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_051_62048_진행중.java
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_051_62048_진행중.java
@@ -1,16 +1,12 @@
 package seohae.algorithm.level2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/62048
  */
 
-public class Lesson_051_62048 {
+public class Lesson_051_62048_진행중 {
     public static void main(String[] args) {
-        Lesson_051_62048 lesson = new Lesson_051_62048();
+        Lesson_051_62048_진행중 lesson = new Lesson_051_62048_진행중();
 
         System.out.println(lesson.solution(8, 12));
     }
--- Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_052_43165.java
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_052_43165.java
@@ -1,21 +1,56 @@
 package seohae.algorithm.level2;
 
+import java.util.Arrays;
+
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/43165
  */
 
 public class Lesson_052_43165 {
+    static int targetValue;
+    static int cnt;
+    static int[] arr;
+    static int[] sign;
+
     public static void main(String[] args) {
         Lesson_052_43165 lesson = new Lesson_052_43165();
 
-        int[] a = new int[]{1,1,1,1};
+        int[] a = new int[]{1,1,1,1,1};
         System.out.println(lesson.solution(a, 3));
     }
 
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+        targetValue = target;
+        arr = numbers;
+        sign = new int[arr.length];
 
+        for (int i = 0; i <numbers.length; i++) {
+            sign[i] = 1;
+        }
 
-        return answer;
+        dfs(0);
+
+        return cnt;
+    }
+
+    static void dfs(int value) {
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += (arr[i] * sign[i]);
+        }
+
+        if (sum == targetValue) {
+            cnt++;
+        }
+
+        /* 재귀함수 */
+        for (int j = value; j < arr.length; j++) {
+            sign[j] = -1;
+            dfs(j + 1);
+
+            sign[j] = 1;
+        }
     }
 }
