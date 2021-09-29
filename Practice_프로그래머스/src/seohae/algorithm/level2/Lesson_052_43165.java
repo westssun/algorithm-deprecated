9a12a83f52c0612263269e880881d5b2b29c28eb
seohae <seohae0406@gmail.com>
2021-09-29 18:29:33 +0900

    0929

---
+++ Practice_프로그래머스/src/seohae/algorithm/level2/Lesson_052_43165.java
@@ -0,0 +1,21 @@
+package seohae.algorithm.level2;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/43165
+ */
+
+public class Lesson_052_43165 {
+    public static void main(String[] args) {
+        Lesson_052_43165 lesson = new Lesson_052_43165();
+
+        int[] a = new int[]{1,1,1,1};
+        System.out.println(lesson.solution(a, 3));
+    }
+
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
