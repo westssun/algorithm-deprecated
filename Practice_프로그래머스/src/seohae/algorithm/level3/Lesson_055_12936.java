c7462b6483ea203383c2f7d9f24e525ce994b69d
seohae <seohae0406@gmail.com>
2021-10-02 10:11:45 +0900

    1003

---
+++ Practice_프로그래머스/src/seohae/algorithm/level3/Lesson_055_12936.java
@@ -0,0 +1,23 @@
+package seohae.algorithm.level3;
+
+import java.util.Arrays;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12936
+ */
+
+public class Lesson_055_12936 {
+    public static void main(String[] args) {
+        Lesson_055_12936 lesson = new Lesson_055_12936();
+
+        System.out.println(Arrays.toString(lesson.solution(3, 5)));
+    }
+
+    public int[] solution(int n, long k) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+}
---
+++ java_algorithm/src/seohae/algorithm/P01_재귀함수/Recursion01.java
@@ -0,0 +1,7 @@
+package seohae.algorithm.P01_재귀함수;
+
+public class Recursion01 {
+    public static void main(String[] args) {
+
+    }
+}
