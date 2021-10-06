ad23cf8add42bb9e206f26c9c5e6f303acfa81f6
seohae <seohae0406@gmail.com>
2021-10-06 20:54:07 +0900

    1006

---
+++ Practice_기타/src/algorithm/D1006/Notion_00001.java
@@ -0,0 +1,86 @@
+package algorithm.D1006;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Code File Number : 00001
+ */
+public class Notion_00001 {
+    public static void main(String[] args) {
+        Notion_00001 lesson = new Notion_00001();
+
+        int[][] a = new int[][] { {100,3}, {200,1}, {50,2} };
+        int[][] b = new int[][] { {2500,3}, {700,5}};
+        System.out.println(lesson.solution(a));
+    }
+
+    public int solution(int[][] arr1) {
+        int answer = 0;
+
+        ArrayList<Integer> coinList = new ArrayList<>();
+        int sum = 0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i][1]; j++) { /* length : 2 */
+                sum += arr1[i][0];
+                coinList.add(arr1[i][0]);
+            }
+        }
+
+        /* 내림차순 정렬 */
+        coinList.sort(Collections.reverseOrder());
+        System.out.println("coinList : " + coinList);
+        System.out.println("sum : " + sum);
+
+        /* 2명으로 나눴을때의 값 */
+        int target = sum / 2;
+        System.out.println("target : " + target);
+
+        int sumA = 0; /* 조카 A */
+        int sumB = 0; /* 조카 B */
+
+        for (int i = 0; i < coinList.size(); i++) {
+            /* target 보다 더한 값이 작을때 덧셈 실행 */
+            if (target >= (sumA + coinList.get(i))) {
+                sumA += coinList.get(i);
+                coinList.set(i, 0); /* 사용했으므로 0으로 값 변경 */
+            } else {
+                System.out.println("sumA : " + sumA);
+                break;
+            }
+        }
+
+        for (int i = 0; i < coinList.size(); i++) {
+            /* target 보다 더한 값이 작을때 덧셈 실행 */
+            if (coinList.get(i) != 0) { /* 위 A 에서 사용되지 않을 경우 */
+                if (target >= (sumB + coinList.get(i))) {
+                    sumB += coinList.get(i);
+                    coinList.set(i, 0); /* 사용했으므로 0으로 값 변경 */
+                } else {
+                    System.out.println("sumB : " + sumB);
+                    break;
+                }
+            }
+        }
+
+        /* 남은 금액 계산 */
+        int finalSum = 0;
+        for (int i = 0; i < coinList.size(); i++) {
+            if (coinList.get(i) != 0) { /* 위 A 에서 사용되지 않을 경우 */
+                finalSum += coinList.get(i);
+            }
+        }
+
+        System.out.println("finalSum : "  + finalSum);
+
+        if (sumA > sumB) {
+            answer = sumA - sumB;
+        } else {
+            answer = sumB - sumA;
+        }
+
+        System.out.println("finalSum - answer :"  + (finalSum - answer));
+        return finalSum - answer;
+    }
+}
